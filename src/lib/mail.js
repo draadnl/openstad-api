78251884765dceb9367904495ce7a29b41e5cd22
Rudi van Hierden <rudi@draad.nu>
2019-10-10 07:39:26 +0200

    Possible fix for attachments not working correctly.

--- src/lib/mail.js
+++ src/lib/mail.js
@@ -177,7 +177,11 @@ function sendSubmissionConfirmationMail( submission, template, emailSubject, sit
         uppercaseHeadings: false
     });
 
-    const attachments = ( site && site.config && site.config.ideas && site.config.ideas.feedbackEmail && site.config.ideas.feedbackEmail.attachments ) || ( config.ideas && config.ideas.feedbackEmail && config.ideas.feedbackEmail.attachments )  || ['logo.png'];
+    const attachments = ( site && site.config && site.config.ideas && site.config.ideas.feedbackEmail && site.config.ideas.feedbackEmail.attachments ) || ( config.ideas && config.ideas.feedbackEmail && config.ideas.feedbackEmail.attachments )  || [{
+        filename : 'logo.png',
+        path     : 'email/img/logo.png',
+        cid      : 'logo'
+    }];
     
     console.log ('====> send mail', data.submission.submittedData.bc_email);
     console.log (attachments);
