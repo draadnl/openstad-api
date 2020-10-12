1df53bd025d58a5dc5ccd0685691686622f5447f
Rudi van Hierden <rudi@draad.nl>
2020-10-12 09:57:24 +0200

    Add route to retrieve form data

--- src/routes/api/submission.js
+++ src/routes/api/submission.js
@@ -6,6 +6,55 @@ const mail = require('../../lib/mail');
 
 let router = express.Router({mergeParams: true});
 
+router.route('/:formId([a-zA-Z0-9\-\_]*)')
+// list submissions
+// --------------
+	.get(auth.can('submissions:list'))
+	.get(function(req, res, next) {
+		let formId = req.params.formId.replace('/', ''),
+		where = { formId };
+		
+		req.scope = ['defaultScope'];
+	//	req.scope.push({method: ['forSiteId', req.params.siteId]});
+
+		db.Submission
+			.scope(...req.scope)
+			.findAll({ where })
+			.then( found => {
+				return found.map( entry => {
+					let data = entry.toJSON();
+					let submittedData = data.submittedData,
+						orderedData = {};
+					
+					// When we get the submittedData back from MySQL, the keys are in the wrong order.
+					// We inserted a number in front of the key before inserting the data, so now we can sort on that
+					Object.keys(submittedData).sort(function (a, b) {
+						let keyA = a.split(':')[0],
+						keyB = b.split(':')[0];
+						
+						if (keyA < keyB) {
+							return -1;
+						} else if (keyA > keyB) {
+							return 1;
+						}
+						
+						return 0;
+					}).forEach(function (key) {
+						var newKey = key.split(':');
+						orderedData[newKey.length > 1 ? newKey[1] : newKey[0]] = submittedData[key];
+					});
+					
+					data.submittedData = orderedData;
+					data.createdAt = moment(data.createdAt).format('LLL');
+					return data;
+				});
+			})
+			.then(function( found ) {
+				res.json(found);
+			})
+			.catch(next);
+	});
+
 router.route('/')
 
 // list submissions
