392deb641c027332400b752a3485cd5e32195022
Rudi van Hierden <rudi@draad.nl>
2020-11-25 16:01:21 +0100

    Fix bug to prevent mails

--- src/lib/mail.js
+++ src/lib/mail.js
@@ -104,7 +104,7 @@ function sendNotificationMail( data ) {
 // send email to user that submitted an idea
 function sendThankYouMail( idea, user, site ) {
   
-  let doSendMail = (site && site.config && site.config.ideas && site.config.ideas.feedbackEmail && site.config.ideas.feedbackEmail.send) || true;
+  let doSendMail = (site && site.config && site.config.ideas && site.config.ideas.feedbackEmail && site.config.ideas.feedbackEmail.send == false);
   
   if (!doSendMail) {
     return;
