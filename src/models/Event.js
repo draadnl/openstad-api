7e34adddacaf41da7bfed642eee95fb6eaf1a88d
Rudi van Hierden <rudi@draad.nl>
2022-10-24 09:32:07 +0200

    fix: allow empty `district`

--- src/models/Event.js
+++ src/models/Event.js
@@ -29,7 +29,7 @@ module.exports = function (db, sequelize, DataTypes) {
 
       district: {
         type: DataTypes.STRING(255),
-        allowNull: false,
+        allowNull: true,
       },
 
       minAge: {
