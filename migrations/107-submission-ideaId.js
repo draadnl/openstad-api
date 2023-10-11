6bbf9741622cc5a63e64df731b96368bcd034927
iandebruin98 <ian@draad.nl>
2023-10-11 15:30:27 +0200

    feat: Pair an ideaId to a submission

---
+++ migrations/107-submission-ideaId.js
@@ -0,0 +1,15 @@
+var db = require('../src/db').sequelize;
+
+module.exports = {
+    up: function() {
+        return db.query(`
+		  ALTER TABLE submissions ADD ideaId int(11) NULL AFTER formId,
+		  ADD KEY \`ideaId\` (\`ideaId\`),
+      ADD CONSTRAINT \`idea_submission\` FOREIGN KEY (\`ideaId\`) REFERENCES \`ideas\` (\`id\`) ON DELETE SET NULL ON UPDATE CASCADE;
+		  
+		`);
+    },
+    down: function() {
+        return db.query(`ALTER TABLE submissions DROP ideaId;`);
+    }
+}
