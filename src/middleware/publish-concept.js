5e3cf15626c345b927434dfe360f39cd035515f1
iandebruin98 <ian@draad.nl>
2023-10-11 15:57:13 +0200

    feat: Change the publish as concept function

--- src/middleware/publish-concept.js
+++ src/middleware/publish-concept.js
@@ -1,9 +1,7 @@
 module.exports = function( req, res, next ) {
   const publishAsConcept = req.body.publishAsConcept;
 
-  if(publishAsConcept === undefined || publishAsConcept === null) {
-    return next();
-  } else if (!publishAsConcept) {
+  if (!publishAsConcept) {
     req.body['publishDate'] = new Date();
   } else {
     req.body['publishDate'] = null;
