d888ba0717b77baefe0e0ce6571d9b61500c5597
iandebruin98 <ian@draad.nl>
2024-01-24 15:34:31 +0100

    feat: Add logs for concept

--- src/middleware/publish-concept.js
+++ src/middleware/publish-concept.js
@@ -1,6 +1,11 @@
 module.exports = function( req, res, next ) {
   const publishAsConcept = req.body.publishAsConcept;
 
+  console.log( 'publishAsConcept START' );
+  console.log( JSON.stringify( req.body ) );
+  console.log( publishAsConcept );
+  console.log( 'publishAsConcept END' );
+
   if(publishAsConcept === undefined || publishAsConcept === null) {
     return next();
   } else if (!publishAsConcept) {
