4393464c97e9911a121828d812a37b3e57b84bd4
iandebruin98 <ian@draad.nl>
2024-02-29 16:00:21 +0100

    feat: Adjust the function that determines whether a publishDate is added or not

--- src/middleware/publish-concept.js
+++ src/middleware/publish-concept.js
@@ -3,9 +3,9 @@ module.exports = function( req, res, next ) {
 
   if(publishAsConcept === undefined || publishAsConcept === null) {
     return next();
-  } else if (!publishAsConcept) {
+  } else if (publishAsConcept === '0') {
     req.body['publishDate'] = new Date();
-  } else {
+  } else if (publishAsConcept === '1') {
     req.body['publishDate'] = null;
   }
     return next();
